55599baae391a3a8f8766207c11260304302de84
Teana Zhong <Teanawk@gmail.com>
2022-11-03 20:35:33 -0400

    commit tree 295fc2e0aab3d485f229839f614ad6d542bf1735

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book 
+    attr_accessor : :author, :page_count, :genre
+    attr_reader :title 
+    def initialize(title)
+        @title = title
+    end 
 
+    def turn_page 
+        puts "The World According to Garp"
+    end 
+end 
 
